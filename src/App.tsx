1bb8f944bebf38a349afc65686300c32cd30460b
Throsby Wells <git@throsby.dev>
2022-06-17 16:35:49 -0400

    pushing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello! Welcome to Throsby's Website</p>
-      <p>If you found this, you should hire him. He's cool.</p>
-    </div>
+    null
   );
 }
 
