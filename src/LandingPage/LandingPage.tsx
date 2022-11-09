2515d5475a83c3c8bdfd0a318b94be4edae79280
Throsby <throsbywells@gmail.com>
2022-11-09 18:00:33 -0500

    fixed elliptical shenanigans

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -5,25 +5,15 @@ function LandingPage(){
         <div className="leftSide">
 
         </div>
-        <div className="vl">
-
-        </div>
+        <div className="vl"/>
         <div className="rightSide"> 
             <div className="outer-circle">
                 <div className="inner-circle"/>
                 <div className="bubbles">
-                    <div className="github-bubble">
-                        Github Link
-                    </div>
-                    <div className="project1-bubble">
-                        Project 1 Link
-                    </div>
-                    <div className="project2-bubble">
-                        Project 2 Link
-                    </div>
-                    <div className="linkedin-bubble">
-                        LinkedIn Link
-                    </div>
+                    <div className="github-bubble">Github Link</div>
+                    <div className="project1-bubble">Project 1 Link</div>
+                    <div className="project2-bubble">Project 2 Link</div>
+                    <div className="linkedin-bubble">LinkedIn Link</div>
                 </div>
             </div>
         </div>
