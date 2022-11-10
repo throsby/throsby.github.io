560e658cbfc754c92cc76c7b0bde02d06193eb35
Throsby <throsbywells@gmail.com>
2022-11-10 17:56:57 -0500

    actually centered now

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -20,6 +20,7 @@ function LandingPage(){
                     <p>I'll do anything, I'll become a champion flamenco dancer for it</p>
                 </div>
                 <div className="item-4">
+                    <p></p>
                     <p>Throsby will win the Steve Martin prize for Banjo excellence only by working hard at it. Also by buying and playing the banjo. Actually mostly that.</p>
                 </div>
             </div>
@@ -29,18 +30,29 @@ function LandingPage(){
             <div className="outer-circle">
                 <div className="inner-circle"/>
                 <div className="bubbles">
-                    <div className="github-bubble">
-                        <a className="bubble-link" href="https://www.github.com/throsby" target="_blank">Github Link</a>
+                    <div className="bubble-outer">
+                        <div className="github-bubble">
+                            <div className="bubble-inner">
+                                <a className="bubble-link" href="https://www.github.com/throsby" target="_blank">Github Link</a>
+                            </div>
+                        </div>
+                        <div className="project1-bubble">
+                            <div className="bubble-inner">
+                                <Link className="bubble-link" to={""}>Project 1 Link</Link>
+                            </div>
+                        </div>
+                        <div className="project2-bubble">
+                            <div className="bubble-inner">
+                                <Link className="bubble-link" to={""}>Project 2 Link</Link>
+                            </div>
+                        </div>
+                        <div className="linkedin-bubble">
+                            <div className="bubble-inner">
+                                <a className="bubble-link" href="https://www.linkedin.com/in/throsbywells/" target="_blank">LinkedIn Link</a>
+                            </div>
+                        </div>
                     </div>
-                    <div className="project1-bubble">
-                        <Link className="bubble-link" to={""}>Project 1 Link</Link>
-                    </div>
-                    <div className="project2-bubble">
-                        <Link className="bubble-link" to={""}>Project 2 Link</Link>
-                    </div>
-                    <div className="linkedin-bubble">
-                        <a className="bubble-link" href="https://www.linkedin.com/in/throsbywells/" target="_blank">LinkedIn Link</a>
-                    </div>
+                
                 </div>
             </div>
         </div>        
