217b122d1bca52bc15822ffc9b8582b8099dbe6a
Throsby <throsbywells@gmail.com>
2023-06-02 13:13:14 -0400

    created link to orto project

--- src/Bubble/Bubble.tsx
+++ src/Bubble/Bubble.tsx
@@ -8,11 +8,14 @@ type Props = {
 }
 
 function Bubble({className, linkTo, openNewWindowOnClick, children}: Props) {
+
+    let destination = linkTo.startsWith("https") ? linkTo : "/about";
+
     return (
     <div className={className}>
         <div className="bubble-inner">
-                {!!openNewWindowOnClick  ? <a className="bubble-link" href={linkTo} target="_blank">{children}</a> 
-                    : <Link className="bubble-link" to={"/about"}>{children}</Link>}
+                {!!openNewWindowOnClick ? <a className="bubble-link" href={linkTo} target="_blank">{children}</a> 
+                    : <a className="bubble-link" href={destination}>{children}</a>}
         </div>
     </div>);  
 };
